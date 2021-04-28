80616defb2267fd715c62ad02afa5d37513e8c22
Damian Wojtowicz <47143782+damianazur@users.noreply.github.com>
2021-04-28 23:45:13 +0100

    Added scripts, changed path, added text

---
+++ GE2 Assignment/Assets/Scripts/Fighter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter : MonoBehaviour
+{
+    public float health = 10;
+    public GameObject bullet;
+    public GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GE2 Assignment/Assets/Scripts/States.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+class PatrolState : State
+{
+    public override void Enter()
+    {
+        owner.GetComponent<FollowPath>().enabled = true;
+    }
+
+    public override void Think()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+        owner.GetComponent<FollowPath>().enabled = false;
+    }
+}
