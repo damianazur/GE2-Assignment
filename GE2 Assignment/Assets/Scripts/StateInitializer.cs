16d9f8fe6db6828e51e180cd57b5541c75f693a6
Damian Wojtowicz <47143782+damianazur@users.noreply.github.com>
2021-04-29 15:13:42 +0100

    Added more existing behaviours, starting on state machines and initial scout returning to station

---
+++ GE2 Assignment/Assets/Scripts/StateInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Reflection;
+
+public class StateInitializer : MonoBehaviour
+{
+    public String initalState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (initalState != null) {
+            Type stateClass = Type.GetType(initalState);
+            GetComponent<StateMachine>().ChangeState((State)Activator.CreateInstance(stateClass));   
+        }  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- GE2 Assignment/Assets/Scripts/States.cs
+++ GE2 Assignment/Assets/Scripts/States.cs
@@ -20,3 +20,57 @@ class PatrolState : State
         owner.GetComponent<FollowPath>().enabled = false;
     }
 }
+
+class DeliverMessage : State
+{
+    public override void Enter()
+    {
+        GameObject[] stopPoints = GameObject.FindGameObjectsWithTag("ScoutStopPoint");
+        GameObject stopPoint = stopPoints[0];
+
+        owner.GetComponent<Seek>().targetGameObject = stopPoint;
+        owner.GetComponent<Seek>().enabled = true;
+    }
+
+    public override void Think()
+    { 
+        GameObject seekTarget = owner.GetComponent<Seek>().targetGameObject;
+        Vector3 targetPos = seekTarget.transform.position;
+        float dist = Vector3.Distance(owner.transform.position, targetPos);
+
+        if (dist < 15.0f) {
+            Debug.Log(seekTarget.transform.position);
+            owner.GetComponent<Arrive>().targetGameObject = seekTarget;
+            owner.GetComponent<Arrive>().enabled = true;
+            owner.GetComponent<Seek>().enabled = false;
+        }
+
+        if (dist < 0.7f) {
+            owner.GetComponent<Arrive>().enabled = false;
+        }
+    }
+
+    public override void Exit()
+    {
+
+    }
+}
+
+
+class GoToDestination : State
+{
+    public override void Enter()
+    {
+
+    }
+
+    public override void Think()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+
+    }
+}
